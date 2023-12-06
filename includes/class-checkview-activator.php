42dc15cf09d7ababb4e64e6759b785c6587df285
Faizan Haidar <faizan@inspry.com>
2023-12-06 11:34:52 +0000

    added basic structure for the plugin

---
+++ includes/class-checkview-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://checkview.io
+ * @since      1.0.0
+ *
+ * @package    Checkview
+ * @subpackage Checkview/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Checkview
+ * @subpackage Checkview/includes
+ * @author     Check View <support@checkview.io>
+ */
+class Checkview_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
