48a9027688108cf3b03c05474b01e67b7d88c17e
Muhammad Faizan Haidar <faizanhaider594@gmail.com>
2024-04-08 16:20:26 +0100

    added fatal-erros logging and turned loggin on by default

--- includes/monitoring/class-checkview-monitoring.php
+++ includes/monitoring/class-checkview-monitoring.php
@@ -9,6 +9,7 @@
  * @subpackage CheckView/includes/monitoring
  */
 
+use Automattic\WooCommerce\Internal\Admin\Logging\FileV2\{ File, FileController, FileListTable, SearchListTable };
 /**
  * Fired to inject custom payment gateway to WooCommerce.
  *
@@ -65,6 +66,13 @@ class Checkview_Monitoring {
 			'checkview_check_wp_config_changes',
 			10,
 			2
+		);
+
+		$this->loader->add_action(
+			'init',
+			$this,
+			'checkview_report_wc_logger',
+			10,
 		);
 	}
 	/**
@@ -261,5 +269,143 @@ class Checkview_Monitoring {
 		}
 
 		return $allowed;
+	}
+
+	/**
+	 * Formats the line for SaaS.
+	 *
+	 * @param string  $line line .
+	 * @param integer $line_number line number.
+	 * @return string
+	 */
+	private function format_line( string $line, int $line_number ): string {
+		$classes = array( 'line' );
+
+		$line = esc_html( $line );
+		if ( empty( $line ) ) {
+			$line = '&nbsp;';
+		}
+
+		$segments      = explode( ' ', $line, 3 );
+		$has_timestamp = false;
+		$has_level     = false;
+
+		if ( isset( $segments[0] ) && false !== strtotime( $segments[0] ) ) {
+			$classes[]     = 'log-entry';
+			$segments[0]   = sprintf(
+				'<span class="log-timestamp">%s</span>',
+				$segments[0]
+			);
+			$has_timestamp = true;
+		}
+
+		if ( isset( $segments[1] ) && WC_Log_Levels::is_valid_level( strtolower( $segments[1] ) ) ) {
+			$segments[1] = sprintf(
+				'<span class="%1$s">%2$s</span>',
+				esc_attr( 'log-level log-level--' . strtolower( $segments[1] ) ),
+				esc_html( WC_Log_Levels::get_level_label( strtolower( $segments[1] ) ) )
+			);
+			$has_level   = true;
+		}
+
+		if ( isset( $segments[2] ) && $has_timestamp && $has_level ) {
+			$message_chunks = explode( 'CONTEXT:', $segments[2], 2 );
+			if ( isset( $message_chunks[1] ) ) {
+				try {
+					$maybe_json = stripslashes( html_entity_decode( trim( $message_chunks[1] ) ) );
+					$context    = json_decode( $maybe_json, false, 512, JSON_THROW_ON_ERROR );
+
+					$message_chunks[1] = sprintf(
+						'<details><summary>%1$s</summary>%2$s</details>',
+						esc_html__( 'Additional context', 'woocommerce' ),
+						wp_json_encode( $context, JSON_PRETTY_PRINT )
+					);
+
+					$segments[2] = implode( ' ', $message_chunks );
+					$classes[]   = 'has-context';
+				} catch ( \JsonException $exception ) { // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedCatch
+					// It's not valid JSON so don't do anything with it.
+				}
+			}
+		}
+
+		if ( count( $segments ) > 1 ) {
+			$line = implode( ' ', $segments );
+		}
+
+		$classes = implode( ' ', $classes );
+
+		return sprintf(
+			'<span id="L%1$d" class="%2$s">%3$s%4$s</span>',
+			absint( $line_number ),
+			esc_attr( $classes ),
+			sprintf(
+				'<a href="#L%1$d" class="line-anchor"></a>',
+				absint( $line_number )
+			),
+			sprintf(
+				'<span class="line-content">%s</span>',
+				wp_kses_post( $line )
+			)
+		);
+	}
+
+	/**
+	 * Reports Wc_logger latest errors.
+	 *
+	 * @param string $log_type log type.
+	 * @return void
+	 */
+	public function checkview_report_wc_logger( $log_type = 'fatal-errors' ) {
+		$is_enabled = get_option( 'woocommerce_logs_logging_enabled', '' );
+		if ( empty( $is_enabled ) || 'yes' !== $is_enabled || null === $is_enabled ) {
+			update_option( 'woocommerce_logs_logging_enabled', 'yes', true );
+		}
+		$file_controller = new FileController();
+		$file_args       = array(
+			'source'         => $log_type, // Use the log type parameter here.
+			'per_page'       => 1,
+			'offset'         => 0,
+			'order'          => 'desc',
+			'orderby'        => 'modified',
+			'posts_per_page' => 1,
+		);
+		$logs            = $file_controller->get_files( $file_args, false );
+		if ( ! is_array( $logs ) || empty( $logs ) ) {
+			return;
+		}
+		// Take the first item as the latest log after sorting.
+		$latest_log = reset( $logs );
+		$file_id    = $latest_log->get_file_id();
+
+		// Update the regular expression to match the log type dynamically.
+		if ( preg_match( '/' . preg_quote( $log_type, '/' ) . '-(\d{4}-\d{2}-\d{2})/', $file_id, $matches ) ) {
+			$date = $matches[1]; // The first captured group, which is the date.
+			// Get the current date in the site's timezone in 'Y-m-d' format.
+			$today = current_time( 'Y-m-d' );
+			if ( $date !== $today ) {
+				return;
+			}
+		}
+		$file        = $file_controller->get_file_by_id( $latest_log->get_file_id() );
+		$stream      = $file->get_stream();
+		$line_number = 1;
+		$errors      = array();
+		$logged      = get_option( $latest_log->get_file_id() );
+		while ( ! feof( $stream ) ) {
+			$line = fgets( $stream );
+			if ( is_string( $line ) ) {
+				if ( $logged && $line_number > $logged ) {
+					$errors[] = $this->format_line( $line, $line_number );
+				} elseif ( ! $logged || empty( $logged ) ) {
+					$errors[] = $this->format_line( $line, $line_number );
+				}
+				++$line_number;
+			}
+		}
+		update_option( $latest_log->get_file_id(), $line_number - 1, true );
+		if ( ! empty( $errors ) ) {
+			update_option( $log_type . '_errors_tracked', $errors ); // Prefix the option name with the log type.
+		}
 	}
 }
