bbb19431881bbbaab1a702b24fe994bc3dbdc325
Muhammad Faizan Haidar <faizanhaider594@gmail.com>
2024-04-15 10:55:23 +0100

    added version changes and api endpoint delete

--- includes/class-checkview.php
+++ includes/class-checkview.php
@@ -79,7 +79,7 @@ class Checkview {
 		if ( defined( 'CHECKVIEW_VERSION' ) ) {
 			$this->version = CHECKVIEW_VERSION;
 		} else {
-			$this->version = '1.1.1';
+			$this->version = '1.1.2';
 		}
 		$this->plugin_name = 'checkview';
 
