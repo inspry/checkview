0a31f979958bad802219d492d1b3fc84c9daa429
Muhammad Faizan Haidar <faizanhaider594@gmail.com>
2024-05-22 14:46:28 +0100

    added version changes

--- includes/class-checkview.php
+++ includes/class-checkview.php
@@ -79,7 +79,7 @@ class Checkview {
 		if ( defined( 'CHECKVIEW_VERSION' ) ) {
 			$this->version = CHECKVIEW_VERSION;
 		} else {
-			$this->version = '1.1.5';
+			$this->version = '1.1.6';
 		}
 		$this->plugin_name = 'checkview';
 
