707c03ba66f4b0724ba79ccbe939431ca9cc3880
MuhammadFaizanHaidar1 <faizan@inspry.com>
2024-12-05 13:38:12 +0000

    added version changes

--- includes/class-checkview.php
+++ includes/class-checkview.php
@@ -79,7 +79,7 @@ class Checkview {
 		if ( defined( 'CHECKVIEW_VERSION' ) ) {
 			$this->version = CHECKVIEW_VERSION;
 		} else {
-			$this->version = '2.0.7';
+			$this->version = '2.0.8';
 		}
 		$this->plugin_name = 'checkview';
 
