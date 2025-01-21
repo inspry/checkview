227adf3694522c949ea509a6eb243b7975369a41
Grayson Gantek <grayson@inspry.com>
2025-01-21 16:17:16 -0500

    Change Webhooks pinger to use environment variable

--- webhooks.sh
+++ webhooks.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-file_url="https://gist.githubusercontent.com/grayson-inspry/90323beb0c4ec526285fd62a04c07c06/raw/e9fb187951fd914954f56abfe87499455c017e23/webhooks.txt"
-temp_file=$(mktemp)
+# Echo webhooks
+echo "$WP_PUSHER_WEBHOOKS"
 
-echo "Downloading file from '$file_url'..."
-curl -s -o "$temp_file" "$file_url"
+# Read URLs from the environment variable, split by newlines
+IFS=$'\n' read -rd '' -a urls <<< "$WP_PUSHER_WEBHOOKS"
 
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to download file."
-  exit 1
-fi
-
-echo "File downloaded successfully."
-
-while IFS= read -r url; do
+# Loop through each URL
+for url in "${urls[@]}"; do
   if [ -z "$url" ]; then
     continue
   fi
@@ -21,7 +15,6 @@ while IFS= read -r url; do
   echo "Requesting: $url"
   response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
   echo "HTTP status code: $response"
-done < "$temp_file"
+done
 
-rm -f "$temp_file"
 echo "Done."
