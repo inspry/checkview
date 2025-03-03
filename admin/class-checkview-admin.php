214c6d09a6ff72cbcf9879222fdbea82833fb260
muhammadfaizanhaidar <faizanhaider594@gmail.com>
2025-03-03 12:43:18 +0000

    added better logs

--- admin/class-checkview-admin.php
+++ admin/class-checkview-admin.php
@@ -356,6 +356,7 @@ class Checkview_Admin {
 			}
 		}
 		if ( ! empty( $cv_test_id ) && ! checkview_is_valid_uuid( $cv_test_id ) ) {
+			Checkview_Admin_Logs::add( 'ip-logs', 'Not Bypassed invalid test id. ' . $visitor_ip . '=> ' . $cv_test_id );
 			return;
 		}
 		if ( $cv_test_id && '' !== $cv_test_id ) {
@@ -378,7 +379,11 @@ class Checkview_Admin {
 				$send_to = CHECKVIEW_EMAIL;
 			}
 		}
-		Checkview_Admin_Logs::add( 'ip-logs', 'Bypassed ' . $visitor_ip . '=> ' . $cv_test_id );
+		if ( empty( $cv_test_id ) ) {
+			Checkview_Admin_Logs::add( 'ip-logs', 'Not Bypassed invalid test id. ' . $visitor_ip . '=> ' . $cv_test_id );
+		} else {
+			Checkview_Admin_Logs::add( 'ip-logs', 'Bypassed ' . $visitor_ip . '=> ' . $cv_test_id );
+		}
 		if ( ! empty( $cv_session ) ) {
 
 			$test_key = $cv_session[0]['test_key'];
